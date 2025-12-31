a88fab3c42a21ad9338af35b301a5b9d01bbd112
Mendy E. <62119241+man-ein@users.noreply.github.com>
2025-12-31 15:48:38 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer-links">
+        <a href="/contact-us.php">Contact Us</a>
+        <a href="/privacy-policy.php">Privacy Policy</a>
+        <a href="/terms-and-conditions.php">Terms & Conditions</a>
+    </div>
+    <div class="copyright">
+        © 2026 findmtl.ca – Strengthening our community, one connection at a time
+    </div>
+</footer>
