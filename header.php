6294e2f22bf96bb0c0a50ecc9378134ba6eef139
Mendy E. <62119241+man-ein@users.noreply.github.com>
2025-12-31 15:48:03 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="header-content">
+        <a href="/" class="logo">FindMTL.ca</a>
+        <div class="nav-wrapper">
+            <nav class="desktop-nav">
+                <a href="/">Home</a>
+                <a href="/about.php">About</a>
+                <a href="/contact-us.php">Contact Us</a>
+            </nav>
+            <div class="hamburger" onclick="toggleMenu()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="nav-menu" id="navMenu">
+                <a href="/shuls/">Shuls & Minyanim</a>
+                <a href="/food/">Kosher Food</a>
+                <a href="/daycare/">Daycare</a>
+                <a href="/boys-schools/">Boys Schools</a>
+                <a href="/girls-schools/">Girls Schools</a>
+                <a href="/organizations/">Organizations</a>
+                <a href="/mikvaot/">Mikvaot</a>
+                <a href="/events/">Events & Shiurim</a>
+                <a href="/countries/">Summer Camps & Countries</a>
+                <a href="/eruv/">Eruv Status</a>
+                <a href="/chevra-kadisha/">Chevra Kadisha</a>
+            </div>
+        </div>
+    </div>
+</header>
